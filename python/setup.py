17d05973005fcfeb5f199ee76e1e32360d990e5a
Patrick Guio <p.guio@ucl.ac.uk>
2026-05-07 16:02:34 +0200

    Modernize code

--- python/setup.py
+++ python/setup.py
@@ -1,27 +1,30 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
-with open('./requirements.txt', 'r') as f:
-    reqs = f.read().splitlines()
-    install_requires = [req.split('=')[0] for req in reqs]
+requirements = Path("requirements.txt").read_text().splitlines()
+
+install_requires = [
+    line.strip()
+    for line in requirements
+    if line.strip() and not line.startswith("#")
+]
 
 setup(
     name="pymagdisc",
+    version="1.0.0",
+    description="Python package for magnetodisc model.",
     author="I Kit Cheng, Dimitrios Millas",
     author_email="i.cheng.19@ucl.ac.uk, dimitrios.millas@ucl.ac.uk",
-    description="Python package for magnetodisc model.",
-    version="1.0",
-    packages=find_packages(exclude=["tests.*", "tests"]),
+
+    packages=find_packages(exclude=["tests", "tests.*"]),
+
     install_requires=install_requires,
-    setup_requires=(
-        'pytest-runner',
-        ),
-    tests_require=(
-            'pytest-cov',
-        ),
 
     entry_points={
-       'console_scripts': [
-           'pymagdisc-plot_mdisc = pymagdisc.vis.plot_magdisc:plot_command',
-       ]
+        "console_scripts": [
+            "pymagdisc-plot_mdisc=pymagdisc.vis.plot_magdisc:plot_command",
+        ]
     },
+
+    python_requires=">=3.9",
 )
