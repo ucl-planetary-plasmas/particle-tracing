4ef6bf67581b813030fb2944a9e90de00db641d1
Patrick Guio <p.guio@ucl.ac.uk>
2026-05-22 15:11:24 +0100

    Fixes and improvments.

--- python/examples/run_mdbtracer.py
+++ python/examples/run_mdbtracer.py
@@ -1,38 +1,30 @@
-
-
 from pathlib import Path
 
 from scipy.io import savemat
 
-from pymagdisc import config
-from pymagdisc.data.load_data import load_model
 from pymagdisc.tracer.mdbtracer import MDBTracer
 
-if __name__ == '__main__':
-
-    mdfile = "jup_mdisc_kh3e7_rmp90.mat" # magnetodisc mat-file
-    partype = "p"                        # proton
-    Ep = 100                             # energy 100 MeV
-    Ri = 4                               # initial equatorial distance 4*Rp
-    ai = 30                              # initial pitch angle 30 degrees
-    timespec = [0,0,2,0]                 # run for 2 dipole bounce periods
-    savefile = "my_jup_mdisc_sim"        # name for result mat-file
-    npertc = 5                           # number of Boris iterations per gyroperiod
-
-    # Build full path to magnetodisc mat-file
-    md_path = Path(config.PATH_TO_DATA) / mdfile
-    MD = load_model(md_path)
+if __name__ == "__main__":
+    mdfile = "jup_mdisc_kh3e7_rmp90.mat"  # magnetodisc mat-file
+    partype = "p"                         # proton
+    Ep = 100                              # energy 100 MeV
+    Ri = 4                                # initial equatorial distance 4*Rp
+    ai = 30                               # initial pitch angle 30 degrees
+    timespec = [0, 0, 2, 0]               # run for 2 dipole bounce periods
+    savefile = "my_jup_mdisc_sim"         # name for result mat-file
+    npertc = 5                            # number of Boris iterations per gyroperiod
 
     # Create particle tracer object
     tracer = MDBTracer(
-       mdfile=mdfile,
-       partype=partype,
-       Ep=Ep,
-       Ri=Ri,
-       ai=ai,
-       timespec=timespec,
-       savefile=savefile,
-       npertc=npertc)
+        mdfile=mdfile,
+        partype=partype,
+        Ep=Ep,
+        Ri=Ri,
+        ai=ai,
+        timespec=timespec,
+        savefile=savefile,
+        npertc=npertc,
+    )
 
     # Run simulation
     results = tracer.run_simul()
@@ -43,9 +35,6 @@ if __name__ == '__main__':
 
     # Save output data
     output_path = results_dir / f"{savefile}.mat"
-    savemat(output_path,results)
+    savemat(output_path, results)
 
-    #print(f"Results saved to: {output_path.resolve()}")
-    print(f"Results saved to: {output_path.relative_to(Path("."))}")
-
-
+    print(f"Results saved to: {output_path.relative_to(Path('.'))}")
