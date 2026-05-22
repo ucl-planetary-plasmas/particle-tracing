b0989ab63e9c98390d6762285778e7ab1d429345
Patrick Guio <p.guio@ucl.ac.uk>
2026-05-22 15:18:25 +0100

    Synced with python version

--- matlab/example_trajectory.m
+++ matlab/example_trajectory.m
@@ -5,11 +5,12 @@ partype = 'S+';                    % proton
 Ep = 25;                         % energy 100 MeV
 Ri = 70;                           % initial equatorial distance 4*Rp
 ai = 10;                          % initial pitch angle 30 degrees
-timespec = [0,0,20,0];             % run for 2 dipole bounce periods
+timespec = [0,0,20,0];             % run for 20 dipole bounce periods
 savefile = [];                    % name for result mat-file
 pauseOn = false;                  % flag to pause on/ pause off.
+plotOn = true;                   % flag to plot on/ plot off.
 npertc = 20;                      % number of Boris iterations per gyroperiod
 
 
-mdbtracer(mdfile,partype,Ep,Ri,ai,timespec,savefile,pauseOn,npertc);
+mdbtracer(mdfile,partype,Ep,Ri,ai,timespec,savefile,pauseOn,plotOn,npertc);
 
