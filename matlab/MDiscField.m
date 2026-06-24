0afa646ce8c85aa91d71a1e640fb54cd3d980ebd
Patrick Guio <p.guio@ucl.ac.uk>
2026-06-24 13:43:53 +0100

    Added curvature and magnetic field length scale parameters.

--- matlab/MDiscField.m
+++ matlab/MDiscField.m
@@ -1,5 +1,5 @@
 function [Br, Bt, varargout] = MDiscField(r, theta)
-% function [Br, Bt, gradB] = MDiscField(r, theta)
+% function [Br, Bt, gradB, curvB] = MDiscField(r, theta)
 %
 % r = radial distance in RJ
 % theta = colatitude in radians
@@ -7,7 +7,9 @@ function [Br, Bt, varargout] = MDiscField(r, theta)
 % Br = radial field in Tesla
 % Bt = meridional field in Tesla
 % gradB = gradient (r,theta) of the amplitude of the magnetic field in
-%         Tesla/m
+%         Tesla/m {dB/dr,1/rdB/dt, gradBm, LB=B/gradBm}
+% curvB = curvature parameters along the field line in m / m-1
+%           {Rc, Kr, Kt, Km, dBr/dr, dBt/dr, 1/rdBr/dt, 1/rdBt/dt} 
 %
 % The disc model filename is set with the global variable MDFile, for
 % instance
@@ -20,7 +22,7 @@ function [Br, Bt, varargout] = MDiscField(r, theta)
 %   clear MDiscField
 % 
 
-% $Id: MDiscField.m,v 1.10 2020/11/17 13:36:45 patrick Exp $
+% $Id: MDiscField.m,v 1.11 2026/06/24 12:42:27 patrick Exp $
 %
 % Copyright (c) 2016-2017 Patrick Guio <patrick.guio@gmail.com>
 % All Rights Reserved.
@@ -39,7 +41,7 @@ function [Br, Bt, varargout] = MDiscField(r, theta)
 % along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 global MDFile DipoleOn
-persistent MD Fr Ft Fm
+persistent MD Fr Ft Fm fr ft
 
 if isempty(MD),
   fprintf(1,'Loading %s ... ', MDFile);
@@ -67,6 +69,12 @@ if isempty(MD),
     B = sqrt(MD.v2d.Br.^2+MD.v2d.Bth.^2)'*MD.scales.bfield;
     Fm = griddedInterpolant(R,Mu,B,opts{:});
 
+		br = Br./B;
+    fr = griddedInterpolant(R,Mu,br,opts{:});
+
+		bt = Bt./B;
+    ft = griddedInterpolant(R,Mu,bt,opts{:});
+
   else, % dipole field
 
     fprintf(1,'dipole field ... '); 
@@ -78,6 +86,12 @@ if isempty(MD),
 
     B = sqrt(MD.v2d.Brdip.^2+MD.v2d.Bthdip.^2)'*MD.scales.bfield;
     Fm = griddedInterpolant(R,Mu,B,opts{:});
+
+		br = Br./B;
+    fr = griddedInterpolant(R,Mu,br,opts{:});
+
+		bt = Bt./B;
+    ft = griddedInterpolant(R,Mu,bt,opts{:});
 
 	end
   fprintf(1,'done.\n');
@@ -100,31 +114,68 @@ Bt = reshape(Ft(X), rs);
 if nargout>2,
 
 EPS = 1e-6;
-rs = size(r);
 
-% radial component
-dr = 2*r(:)*EPS * MD.scales.length;
-rp = r(:)*(1+EPS);
-rm = r(:)*(1-EPS);
+% radial component d/dr
+dr = 2*r(:)*EPS * MD.scales.length; % [m]
+rp = r(:)*(1+EPS); % normalised
+rm = r(:)*(1-EPS); % normalised
 % one argument seems faster than two arguments
 Xp = [rp, mu(:)];
 Xm = [rm, mu(:)];
-dBdr = reshape((Fm(Xp)-Fm(Xm))./dr,rs);
+dBdr = reshape((Fm(Xp)-Fm(Xm))./dr,rs); % [T/m]
 %dBdr = reshape((Fm(rp,mu(:))-Fm(rm,mu(:)))./dr,rs);
 
-% tangential component
+if nargout>2, % B for gradB 
+
+B = reshape(Fm(X), rs);
+
+if nargout>3, % radial components for curvB
+
+br = reshape(fr(X), rs);
+bt = reshape(ft(X), rs);
+
+dbrdr = reshape((fr(Xp)-fr(Xm))./dr,rs); % [m-1]
+dbtdr = reshape((ft(Xp)-ft(Xm))./dr,rs); % [m-1]
+
+end
+end
+
+% tangential component 1/r d/dt
 dmudt = -sqrt(1-mu(:).^2);
 % add EPS to deal with mu=0
 mup = mu(:)*(1+EPS)+EPS;
 mum = mu(:)*(1-EPS)-EPS;
-rdmu = r(:).*(mup-mum) * MD.scales.length;
+rdmu = r(:).*(mup-mum) * MD.scales.length; % [m]
 % one argument seems faster than two arguments
 Xp = [r(:), mup];
 Xm = [r(:), mum];
-dBdt = reshape((Fm(Xp)-Fm(Xm))./rdmu.*dmudt,rs);
+dBdt = reshape((Fm(Xp)-Fm(Xm))./rdmu.*dmudt,rs); % [T/m]
 %dBdt = reshape((Fm(r(:),mup)-Fm(r(:),mum))./rdmu.*dmudt,rs);
 
-varargout{1} = {dBdr, dBdt};
+gradBm = sqrt(dBdr.^2+dBdt.^2);
+LB = B./gradBm;
+
+gradB = {dBdr, dBdt, gradBm, LB};
+
+varargout{1} = gradB;
+
+if nargout>3, % tangential components for curvB
+
+dbrdt = reshape((fr(Xp)-fr(Xm))./rdmu.*dmudt,rs); % [m-1]
+dbtdt = reshape((ft(Xp)-ft(Xm))./rdmu.*dmudt,rs); % [m-1]
+
+Kr = br.*dbrdr + bt.*dbrdt - bt.^2./(r*MD.scales.length);
+Kt = br.*dbtdr + bt.*dbtdt + br.*bt./(r*MD.scales.length);
+
+Km = sqrt(Kr.^2+Kt.^2);
+
+Rc = 1./Km;
+
+curvB = {Rc, Kr, Kt, Km, dbrdr, dbrdt, dbtdr, dbtdt};
+
+varargout{2} = curvB;
+
+end
 
 end
 
