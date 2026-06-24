583510e2cbf068cb5cfd7ead61ad6906b93bd29d
Patrick Guio <p.guio@ucl.ac.uk>
2026-06-24 22:44:23 +0100

    Cosmetic changes.

--- matlab/example_trajectory.m
+++ matlab/example_trajectory.m
@@ -1,15 +1,15 @@
 function example_trajectory
 
 mdfile = '../data/jup_mdisc_kh3e7_rmp90'; % magnetodisc mat-file
-partype = 'S+';                    % proton
-Ep = 25;                         % energy 100 MeV
-Ri = 70;                           % initial equatorial distance 4*Rp
-ai = 10;                          % initial pitch angle 30 degrees
-timespec = [0,0,20,0];             % run for 20 dipole bounce periods
-savefile = [];                    % name for result mat-file
-pauseOn = false;                  % flag to pause on/ pause off.
-plotOn = true;                   % flag to plot on/ plot off.
-npertc = 20;                      % number of Boris iterations per gyroperiod
+partype = 'S+';                           % proton
+Ep = 25;                                  % energy [MeV]
+Ri = 70;                                  % initial equatorial distance [Rp]
+ai = 10;                                  % initial equatorial pitch angle [deg]
+timespec = [0,0,20,0];                    % run for 20 dipole bounce periods
+savefile = [];                            % name for result mat-file
+pauseOn = true;                           % flag to pause on/off.
+plotOn = true;                            % flag to plot on/off.
+npertc = 20;                              % number of iterations/equatorial gyroperiod
 
 
 mdbtracer(mdfile,partype,Ep,Ri,ai,timespec,savefile,pauseOn,plotOn,npertc);
