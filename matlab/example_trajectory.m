28b878677ac8f028deb96b8a17b7b19aa107ceee
Patrick Guio <p.guio@ucl.ac.uk>
2026-07-08 19:53:30 +0100

    Added copyright and fixe mdisc path.

--- matlab/example_trajectory.m
+++ matlab/example_trajectory.m
@@ -1,6 +1,26 @@
 function example_trajectory
+% function example_trajectory
 
-mdfile = '../data/jup_mdisc_kh3e7_rmp90'; % magnetodisc mat-file
+%
+% $Id: example_trajectory.m,v 1.2 2026/07/08 18:52:48 patrick Exp $
+%
+% Copyright (c) 2025 Patrick Guio <patrick.guio@gmail.com>
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 3 of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+mdfile = 'jup_mdisc_kh3e7_rmp90'; % magnetodisc mat-file
 partype = 'S+';                           % proton
 Ep = 25;                                  % energy [MeV]
 Ri = 70;                                  % initial equatorial distance [Rp]
